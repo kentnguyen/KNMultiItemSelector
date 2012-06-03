3963ce6bfcc9b40d41860f1d2e764c00f845a195
Kent Nguyen <nguyen.dmz@gmail.com>
2012-06-03 14:46:03 +0800

    Setup basic Facebook app

--- KNFBFriendSelectorDemo/KNFBAppDelegate.h
+++ KNFBFriendSelectorDemo/KNFBAppDelegate.h
@@ -7,11 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "FBConnect.h"
 
-@interface KNFBAppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+@interface KNFBAppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate, FBSessionDelegate> {
+  Facebook * facebook;
+}
 
 @property (strong, nonatomic) UIWindow *window;
-
+@property (strong, nonatomic) Facebook *facebook;
 @property (strong, nonatomic) UITabBarController *tabBarController;
 
 @end
--- KNFBFriendSelectorDemo/KNFBAppDelegate.m
+++ KNFBFriendSelectorDemo/KNFBAppDelegate.m
@@ -15,60 +15,70 @@
 @implementation KNFBAppDelegate
 
 @synthesize window = _window;
+@synthesize facebook;
 @synthesize tabBarController = _tabBarController;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+  self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+
+  // Init Facebook
+  facebook = [[Facebook alloc] initWithAppId:@"316629235082530" andDelegate:self];
+  NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+  if ([defaults objectForKey:@"FBAccessTokenKey"] && [defaults objectForKey:@"FBExpirationDateKey"]) {
+    facebook.accessToken = [defaults objectForKey:@"FBAccessTokenKey"];
+    facebook.expirationDate = [defaults objectForKey:@"FBExpirationDateKey"];
+  }
+  if (![facebook isSessionValid]) {
+    [SVProgressHUD show];
+    [facebook authorize:[NSArray arrayWithObjects:@"user_about_me",@"friends_about_me", nil]];
+  }
+
+  // Override point for customization after application launch.
   UIViewController *viewController1 = [[KNFBFirstViewController alloc] initWithNibName:@"KNFBFirstViewController" bundle:nil];
   UIViewController *viewController2 = [[KNFBSecondViewController alloc] initWithNibName:@"KNFBSecondViewController" bundle:nil];
   self.tabBarController = [[UITabBarController alloc] init];
   self.tabBarController.viewControllers = [NSArray arrayWithObjects:viewController1, viewController2, nil];
   self.window.rootViewController = self.tabBarController;
-    [self.window makeKeyAndVisible];
-    return YES;
+  [self.window makeKeyAndVisible];
+
+  return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-  // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-  // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-  // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-  // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-  // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
+- (void)applicationDidBecomeActive:(UIApplication *)application {
   // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-  // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+#pragma mark - Facebook Session Delegate
+
+- (void)fbDidLogin {
+  NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+  [defaults setObject:[facebook accessToken] forKey:@"FBAccessTokenKey"];
+  [defaults setObject:[facebook expirationDate] forKey:@"FBExpirationDateKey"];
+  [defaults synchronize];
+  [SVProgressHUD dismissWithSuccess:@"Facebook"];
 }
 
-/*
-// Optional UITabBarControllerDelegate method.
-- (void)tabBarController:(UITabBarController *)tabBarController didSelectViewController:(UIViewController *)viewController
-{
+- (void)fbDidNotLogin:(BOOL)cancelled {
+  [SVProgressHUD dismissWithError:nil];
 }
-*/
 
-/*
-// Optional UITabBarControllerDelegate method.
-- (void)tabBarController:(UITabBarController *)tabBarController didEndCustomizingViewControllers:(NSArray *)viewControllers changed:(BOOL)changed
-{
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+  return [facebook handleOpenURL:url]; 
 }
-*/
+
+- (void)fbDidLogout {}
+- (void)fbSessionInvalidated {}
+
+#pragma mark - Extending Facebook Token
+
+-(void)fbDidExtendToken:(NSString *)accessToken expiresAt:(NSDate *)expiresAt {
+  NSLog(@"TOKEN EXTENDED");
+  NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+  [defaults setObject:accessToken forKey:@"FBAccessTokenKey"];
+  [defaults setObject:expiresAt forKey:@"FBExpirationDateKey"];
+  [defaults synchronize];
+}
 
 @end
