d84ef2b222bca44f2650ed8525856e0fc98b3280
Kent Nguyen <nguyen.dmz@gmail.com>
2012-06-04 17:25:08 +0800

    Add some documentation

--- KNFBFriendSelectorDemo/KNFBFirstViewController.m
+++ KNFBFriendSelectorDemo/KNFBFirstViewController.m
@@ -26,10 +26,13 @@
   }
   return self;
 }
+
+#pragma mark - Fetch Facebook friends
 							
 - (void)viewDidLoad {
   [super viewDidLoad];
 
+  // Fetch all facebook friends and store in NSArray
   NSString * ogEndpoint = [NSString stringWithFormat:@"https://graph.facebook.com/me/friends?access_token=%@", ApplicationDelegate.facebook.accessToken];
   ASIHTTPRequest * request = [ASIHTTPRequest requestWithURL:[NSURL URLWithString:ogEndpoint]];
   [request setCompletionBlock:^{
@@ -41,7 +44,7 @@
     for (NSDictionary * f in dataArray) {
       [friends addObject:[[KNSelectorItem alloc] initWithDisplayValue:[f objectForKey:@"name"]
                                                           selectValue:[f objectForKey:@"id"]
-                                                             imageUrl:[NSString stringWithFormat:@"http://graph.facebook.com/%@/picture?type=normal", [f objectForKey:@"id"]]]];
+                                                             imageUrl:[NSString stringWithFormat:@"http://graph.facebook.com/%@/picture?type=square", [f objectForKey:@"id"]]]];
     }
     [friends sortUsingSelector:@selector(compareByDisplayValue:)];
   }];
@@ -56,6 +59,8 @@
   return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
+#pragma mark - Presenting the selector
+
 - (IBAction)pickerButtonDidTouch:(id)sender {
   KNMultiItemSelector * selector = [[KNMultiItemSelector alloc] initWithItems:friends
                                                              preselectedItems:nil
@@ -66,13 +71,22 @@
   [self presentModalViewController:uinav animated:YES];
 }
 
+#pragma mark - Handle delegate callback
+
 -(void)selectorDidCancelSelection {
   [self dismissModalViewControllerAnimated:YES];
 }
 
 -(void)selectorDidFinishSelectionWithItems:(NSArray *)selectedItems {
   [self dismissModalViewControllerAnimated:YES];
+
+  textView.text = @"You have selected:\n";
+  for (KNSelectorItem * i in selectedItems) {
+    textView.text = [textView.text stringByAppendingFormat:@"- ID %@\n", i.selectValue];
+  }
 }
+
+#pragma mark - Memory
 
 - (void)viewDidUnload {
   [self setPickerButton:nil];
